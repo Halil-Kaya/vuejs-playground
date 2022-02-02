5e50d2b212ab24921b1d042c44b515c8e6490214
Halil İbrahim Kaya <halil.kaya@naylalabs.com>
2022-02-02 21:09:07 +0300

    Add 2-vue-instance project to firs-steps

---
+++ first-steps/2-vue-instance/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title : "First steps",
+            content : "Contents",
+            eduflow:{
+                title : "click me",
+                target:"target_",
+                url:"https://google.com",
+                alt:"google",
+            },
+            owner : "Halil Kaya",
+            cords:{
+                x : 0,
+                y: 0
+            }
+        }
+    },
+    methods:{
+        changeTitle(msg){
+            this.title=msg
+        },
+        updateCoords(message,event){
+            this.cords.x = event.x;
+            this.cords.y = event.y;
+        },
+        testBtn(event){
+            console.log(event)
+        }
+    }
+}).mount('#app')
