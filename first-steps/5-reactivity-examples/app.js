a4066faaa2858c58c3a85a80ce819e71070964f5
Halil Ibrahim Kaya <54222374+Halil-Kaya@users.noreply.github.com>
2022-02-21 21:29:29 +0300

    Add reactivity project.

---
+++ first-steps/5-reactivity-examples/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            search : "",
+            itemList : [
+                "elma",
+                "armut",
+                "kiraz",
+                "çilek"
+            ],
+            //filteredList : []
+        }
+    },
+    methods:{
+        searchList(){
+            //this.filteredList = this.itemList.filter(i => i.includes(this.search))
+        }
+    },
+    computed:{
+        filteredList(){
+            return this.itemList.filter(i => i.includes(this.search))
+        }
+    }
+}).mount('#app')
